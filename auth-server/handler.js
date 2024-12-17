db9104448912b8d28362f22bec5bc83567e064d9
plumbz <plumby.eschenbacher@yahoo.com>
2024-12-17 23:16:00 +0100

    fixed the token function

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -23,46 +23,6 @@ module.exports.getAuthURL = async () => {
    * Scopes array is passed to the `scope` option.
    *
    */
-
-
-  module.exports.getAccessToken = async (event) => {
-    // Decode authorization code extracted from the URL query
-    const code = decodeURIComponent(`${event.pathParameters.code}`);
-
-
-    return new Promise((resolve, reject) => {
-      /**
-       *  Exchange authorization code for access token with a “callback” after the exchange,
-       *  The callback in this case is an arrow function with the results as parameters: “error” and “response”
-       */
-
-
-      oAuth2Client.getToken(code, (error, response) => {
-        if (error) {
-          return reject(error);
-        }
-        return resolve(response);
-      });
-    })
-      .then((results) => {
-        // Respond with OAuth token
-        return {
-          statusCode: 200,
-          headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Credentials': true,
-          },
-          body: JSON.stringify(results),
-        };
-      })
-      .catch((error) => {
-        // Handle error
-        return {
-          statusCode: 500,
-          body: JSON.stringify(error),
-        };
-      });
-  };
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: "offline",
     scope: SCOPES,
@@ -81,3 +41,41 @@ module.exports.getAuthURL = async () => {
   };
 };
 
+module.exports.getAccessToken = async (event) => {
+  // Decode authorization code extracted from the URL query
+  const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+
+  return new Promise((resolve, reject) => {
+    /**
+     *  Exchange authorization code for access token with a “callback” after the exchange,
+     *  The callback in this case is an arrow function with the results as parameters: “error” and “response”
+     */
+
+
+    oAuth2Client.getToken(code, (error, response) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve(response);
+    });
+  })
+    .then((results) => {
+      // Respond with OAuth token
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify(results),
+      };
+    })
+    .catch((error) => {
+      // Handle error
+      return {
+        statusCode: 500,
+        body: JSON.stringify(error),
+      };
+    });
+};
