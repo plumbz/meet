9ded4f5156101cd39c6cbde829847366f11ba826
plumbz <plumby.eschenbacher@yahoo.com>
2025-01-05 15:54:46 +0100

    safety commit

--- src/components/CitySearch.jsx
+++ src/components/CitySearch.jsx
@@ -2,18 +2,18 @@ import React, { useState } from 'react';
 
 
 const CitySearch = () => {
-    const [showSuggestions, setShowSuggestions] = useState(false);
-    return (
-        <div id="city-search">
-            <input
-                type="text"
-                className="city"
-                    placeholder="Search for a City"
-                onFocus={() => setShowSuggestions(true)}
-            />
-            {showSuggestions ? <ul className="suggestions"></ul> : null}
-        </div>
-    )
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  return (
+    <div id="city-search">
+      <input
+        type="text"
+        className="city"
+        placeholder="Search for a city"   
+        onFocus={() => setShowSuggestions(true)}
+      />
+      {showSuggestions ? <ul className="suggestions"></ul> : null}
+    </div>
+  )
 }
 
 export default CitySearch;
