0c75e2f2c8b0e1fd2d33b2c16266d1b1d1814698
plumbz <plumby.eschenbacher@yahoo.com>
2025-01-28 20:20:26 +0100

    installed workbox, added icons, service-worker and serviceRegistration

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import * as atatus from 'atatus-spa';
+//import * as atatus from 'atatus-spa';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
-atatus.config('c82cd67b44cf4f069f97fe0e76b1e070').install();
+
+
+//atatus.config('c82cd67b44cf4f069f97fe0e76b1e070').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+ );
+ serviceWorkerRegistration.register();
