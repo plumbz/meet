76e3472c6b379c12f43045be7b66a0f4fa769fde
plumbz <plumby.eschenbacher@yahoo.com>
2025-02-08 21:34:31 +0100

    fixed CityEventsChart scatter

--- src/components/CityEventsChart.jsx
+++ src/components/CityEventsChart.jsx
@@ -9,22 +9,21 @@ import {
 } from 'recharts';
 
 const CityEventsChart = ({ allLocations, events })  => {
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-    useEffect(() => {
-        setData(getData());
-    }, [`${data}`]);
+  useEffect(() => {
+      setData(getData());
+  }, [`${events}`, `${allLocations}`]);
 
-    const getData = () => {
-        const data = allLocations.map((location) => {
-        const count = events.filter((event) => event.location === location).length
-          const city = location.split((/, | - /))[0]
-          return { city, count };
-        })
-        return data;
-    };
+  const getData = () => {
+    const data = allLocations.map((location) => {
+      const count = events.filter((event) => event.location === location).length
+      const city = location.split((/, | - /))[0]
+      return { city, count };
+    })
+    return data;
+  };
     
-
   return (
     <ResponsiveContainer width="99%" height={400}>
       <ScatterChart
@@ -35,15 +34,15 @@ const CityEventsChart = ({ allLocations, events })  => {
           left: -30,
         }}
       >
-        <CartesianGrid />
-        <XAxis
-          type="category" dataKey="city" name="City"
-          angle={60} interval={0} tick={{ dx: 20, dy: 40, fontSize: 14 }}
+      <CartesianGrid />
+      <XAxis
+        type="category" dataKey="city" name="City"
+        angle={60} interval={0} tick={{ dx: 20, dy: 40, fontSize: 14 }}
         />
-        <YAxis type="number" dataKey="countnumber" name="Number of events" allowDecimals={false} />
-        <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-        <Scatter name="A school" data={data} fill="#8884d8" />
-      </ScatterChart>
+      <YAxis type="number" dataKey="count" name="Number of events" allowDecimals={false} />
+      <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+      <Scatter name="A school" data={data} fill="#8884d8" />
+    </ScatterChart>
     </ResponsiveContainer>
   );
 }
